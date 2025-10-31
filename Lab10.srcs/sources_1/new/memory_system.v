9fa0c7ad332bf18dec44ebc620e475c9571c45ba
Rylee Hilde <ryleehilde@u.boisestate.edu>
2025-10-30 18:45:44 -0600

    Implemented memory system; TODO check syntax and make sure all the data is connected correctly

--- Lab10.srcs/sources_1/new/memory_system.v
+++ Lab10.srcs/sources_1/new/memory_system.v
@@ -10,9 +10,23 @@ module memory_system(
     // data and store into the one selected by
     // addr
     
-    demultiplexer demux (.data(data),.sel(addr),.A(),.B(),.C(),.D());
+    wire [7:0] arr_vec[3:0]; //stores 4 8-bit signals of data
     
-    byte_memory A (.data(data),);
+    wire str[3:0]; //store
+    
+    //demux data
+    demultiplexer demux (.data(data),.sel(addr),.A(arr_vec[0]),.B(arr_vec[1]),.C(arr_vec[2]),.D(arr_vec[3]));
+    
+    //demux store
+    store_demux enable_demux (.data(store),.sel(addr),.A(str[0]),.B(str[1]),.C(str[2]),.D(str[3]));
+    
+    //The 4 bytes of memory
+    byte_memory A (.data(data),.store(str[0]),.memory(arr_vec[0]));
+    byte_memory B (.data(data),.store(str[1]),.memory(arr_vec[1]));
+    byte_memory C (.data(data),.store(str[2]),.memory(arr_vec[2]));
+    byte_memory D (.data(data),.store(str[3]),.memory(arr_vec[3]));
+
+    multiplexer mux (.A(arr_vec[0]),.B(arr_vec[1]),.C(arr_vec[2]),.D(arr_vec[3]),.Sel(addr),.Y(memory));
 
     // It should then multiplex the output of the
     // memory specified by addr into the memory
